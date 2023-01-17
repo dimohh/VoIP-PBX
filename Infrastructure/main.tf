df5b7a1acbcbc380b16a1267320e498c9dc24b16
dimohh <115533782+dimohh@users.noreply.github.com>
2023-01-17 10:06:46 +0200

    Update main.tf

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,1 +1,52 @@
+# Deploy S3 bucket for storing remote state file
 
+resource "aws_s3_bucket" "terraform_backend_bucket" {
+  bucket = "asterisk-terraform-backend"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+      prevent_destroy = true 
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# Deploy DynamoDB table as state locking mechanism
+
+resource "aws_dynamodb_table" "terraform_backend_table" {
+  name           = "asterisk-tf-state-locking"
+  hash_key       = "LockID"
+  read_capacity  = 5
+  write_capacity = 5
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+# Terraform behavior attributes
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Default AWS provider
+
+provider "aws" {
+  region    = "eu-west-1"
+}
