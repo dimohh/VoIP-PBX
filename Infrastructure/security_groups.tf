35d279f2cff8969b9cf0592425c5a0dd9f24c3e3
dimohh <115533782+dimohh@users.noreply.github.com>
2023-01-20 19:09:12 +0200

    Create security_groups.tf

---
+++ Infrastructure/security_groups.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "asterisk_sg" {
+  name        = "Asterisk security group"
+  description = "Asterisk access rules"
+  vpc_id      = aws_vpc.asterisk_vpc.id
+
+  ingress {
+    description = "SSH from Internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+    ingress {
+    description = "SIP from Internet"
+    from_port   = 5060
+    to_port     = 5060
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SIP from Internet"
+    from_port   = 5060
+    to_port     = 5060
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" = "Asterisk access rules"
+  }
+}
